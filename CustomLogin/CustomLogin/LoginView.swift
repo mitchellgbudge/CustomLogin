ce01576d6f7f989c8424edcf36d2aabfa8337aa0
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-11 21:25:37 -0700

    Create UIControl and helper files

---
+++ CustomLogin/CustomLogin/LoginView.swift
@@ -0,0 +1,15 @@
+//
+//  LoginView.swift
+//  CustomLogin
+//
+//  Created by Mitchell Budge on 11/11/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIControl {
+
+    
+
+}
---
+++ CustomLogin/CustomLogin/StrengthEnum.swift
@@ -0,0 +1,15 @@
+//
+//  StrengthEnum.swift
+//  CustomLogin
+//
+//  Created by Mitchell Budge on 11/11/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+enum Strength: Int {
+    case weak = 1
+    case medium
+    case strong
+}
