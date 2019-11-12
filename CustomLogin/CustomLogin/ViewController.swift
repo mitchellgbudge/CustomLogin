953ef6676cc3d8331a5ff4e69e0951e2e8054ec8
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-11 21:23:33 -0700

    Add base Xcode project and configure UI

---
+++ CustomLogin/CustomLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomLogin
+//
+//  Created by Mitchell Budge on 11/11/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
