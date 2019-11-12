c93c71a13f7fcf2a2307d45ba4e8ad35e7416d2f
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-11 21:53:42 -0700

    Add text view delegate methods

--- CustomLogin/CustomLogin/LoginView.swift
+++ CustomLogin/CustomLogin/LoginView.swift
@@ -40,7 +40,57 @@ class LoginView: UIControl {
     let textFieldBorderColor = UIColor(hue: 208/360.0, saturation: 80/100.0, brightness: 94/100.0, alpha: 1)
     let bgColor = UIColor(hue: 0, saturation: 0, brightness: 97/100.0, alpha: 1)
     
+    // MARK: - Setup
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        createUI()
+    }
+    
+    func createUI() {
+        
+    }
+    
+    
+    // MARK: - Helper Functions
+    func determineStrength(text: String) {
+        var strength = Strength.weak
+        switch text.count {
+        case 0...9:
+            strength = .weak
+        case 10...19:
+            strength = .medium
+        default:
+            strength = .strong
+        }
+        updateViews(strength: strength)
+    }
+    
+    func updateViews(strength: Strength) {
+        
+    }
+    
+}
 
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let originalText = textField.text!
+        let textRange = Range(range, in: originalText)!
+        let replacedText = originalText.replacingCharacters(in: textRange, with: string)
+        determineStrength(text: replacedText)
+        return true
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text {
+            password = text
+        }
+        sendActions(for: .valueChanged)
+    }
 }
